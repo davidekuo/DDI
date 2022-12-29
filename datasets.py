cefd39f97c812829dadd383c75d12f9425dd0a8c
David Kuo <dekuo@stanford.edu>
2022-12-28 22:30:28 -0800

    updated datasets.py, train.py; added run_experiment.ipynb to train via Colab

--- datasets.py
+++ datasets.py
@@ -1,15 +1,15 @@
 import os
 import pandas as pd
 import numpy as np
-from torch.utils.data import Dataset, random_split #Subset
+import torch
+import pytorch_lightning as pl
+from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision import transforms as T
 from torchvision.io import read_image
-import pytorch_lightning as pl
-
 
 """Constants and transforms copied from DDI-Code"""
 means = [0.485, 0.456, 0.406]
-stds  = [0.229, 0.224, 0.225]
+stds = [0.229, 0.224, 0.225]
 test_transform = T.Compose([
     lambda x: x.convert('RGB'),
     T.Resize(299),
@@ -20,7 +20,7 @@ test_transform = T.Compose([
 
 
 def params_to_list(params, defaults: list):
-  """
+    """
   Helper function for DDI_DataModule class
 
   Args:
@@ -30,34 +30,35 @@ def params_to_list(params, defaults: list):
   Returns:
     list of parameters
   """
-  if params is None:
-    return defaults
-  else:
-    return params if isinstance(params, list) else [params]
+    if params is None:
+        return defaults
+    else:
+        return params if isinstance(params, list) else [params]
 
 
 class DDI_Dataset(Dataset):
-  """
+    """
   PyTorch Dataset class for Diverse Dermatology Images dataset
   """
-  def __init__(self, annotations_df, img_dir, transform=None):
-    """
+
+    def __init__(self, annotations_df, img_dir, transform=None):
+        """
     Args:
       - annotations_df (Pandas DataFrame): DataFrame of annotations for dataset
       - img_dir (str): path to directory storing DDI images
       - transform (torchvision.transforms): torchvision transforms for dataset images
     """
-    super().__init__()
-    self.annotations = annotations_df
-    self.img_dir = img_dir  # './ddi_data/'
-    self.transform = transform
+        super().__init__()
+        self.annotations = annotations_df
+        self.img_dir = img_dir  # './ddi_data/'
+        self.transform = transform
 
-  def __len__(self):
-    """Returns size of dataset"""
-    return len(self.annotations)
+    def __len__(self):
+        """Returns size of dataset"""
+        return len(self.annotations)
 
-  def __getitem__(self, idx):
-    """
+    def __getitem__(self, idx):
+        """
     For idx-th data point, returns:
       - image (Tensor): PyTorch tensor representation of DDI image
       - skin_tone (int): FST category [12, 34, 56]
@@ -65,33 +66,33 @@ class DDI_Dataset(Dataset):
       - disease (string): diagnosis of lesion
       - disease_label (int): int label corresponding to disease
     """
-    img_path = self.img_dir + os.sep + self.annotations.loc[idx, 'DDI_file'] # str
-    image = read_image(img_path) # Tensor
-    skin_tone = self.annotations.loc[idx, 'skin_tone'] # int
-    malignant = self.annotations.loc[idx, 'malignant'] # boolean
-    disease = self.annotations.loc[idx, 'disease'] # str
-    disease_label = self.annotations.loc[idx, 'disease_label'] # int
-    
-    if self.transform:
-      image = self.transform(image)
-    
-    return image, skin_tone, malignant, disease, disease_label
+        img_path = self.img_dir + os.sep + self.annotations.loc[idx, 'DDI_file']  # str
+        image = read_image(img_path)  # Tensor
+        skin_tone = self.annotations.loc[idx, 'skin_tone']  # int
+        malignant = self.annotations.loc[idx, 'malignant']  # boolean
+        disease = self.annotations.loc[idx, 'disease']  # str
+        disease_label = self.annotations.loc[idx, 'disease_label']  # int
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, skin_tone, malignant, disease, disease_label
 
 
 class DDI_DataModule(pl.LightningDataModule):
-  """LightningDataModule for Diverse Dermatology Images dataset"""
+    """LightningDataModule for Diverse Dermatology Images dataset"""
 
-  def __init__(self, 
-               random_seed,
-               batch_size,
-               num_workers,
-               metadata_csv, 
-               img_dir,
-               transform=None,  
-               skin_tone=None, 
-               malignant=None, 
-               disease=None):
-    """ 
+    def __init__(self,
+                 random_seed,
+                 batch_size,
+                 num_workers,
+                 metadata_csv,
+                 img_dir,
+                 transform=None,
+                 skin_tone=None,
+                 malignant=None,
+                 diseases=None):
+        """
     Args:
       - random_seed (int): random seed for generating train/val/test splits
       - batch_size (int): batch size for DataLoader
@@ -103,66 +104,66 @@ class DDI_DataModule(pl.LightningDataModule):
       - malignant (list[bool]): malignancy categories to include. None -> include both malignant and benign
       - diseases (list[str]): diagnoses to include. None -> include all diagnoses 
     """
-    super().__init__()
-    # parameters for DataLoader
-    self.seed = random_seed
-    self.batch_size = batch_size
-    self.num_workers = num_workers
-    
-    # parameters for DDI_DataSet
-    self.metadata = pd.read_csv(metadata_csv, index_col=0) # './ddi_data/ddi_metadata.csv'
-    self.img_dir = img_dir  # './ddi_data/'
-    self.transform = transform
+        super().__init__()
+        # parameters for DataLoader
+        self.seed = random_seed
+        self.batch_size = batch_size
+        self.num_workers = num_workers
 
-    # Create dictionary mapping disease (str) to label (int)
-    self.disease2label = {disease:label for label, disease in enumerate(sorted(annotations['disease'].unique()))}
-    # Add 'disease_label' col to metadata with int label for each diagnosis
-    self.metadata['disease_label'] = self.metadata['disease'].map(disease2label)
-    
-    # Create DDI_Dataset of specified skin tones, malignancy status, and diagnoses
-    # 1. parse inputs into lists for downstream processing
-    skin_tone = params_to_list(skin_tone, [12, 34, 56])
-    malignant = params_to_list(malignant, [True, False])
-    diseases = params_to_list(diseases, list(self.disease2label))
-    # 2. make sure specified inputs are valid
-    for s in skin_tone:
-      assert s in [12, 34, 56], f"{s} is not a valid skin tone"
-    for d in diseases:
-      assert d in self.disease2label, f"{d} is not a valid diagnosis"
-    # 3. select rows of metadata DataFrame that meet specifications
-    indices = np.where(self.metadata['skin_tone'].isin(skin_tone) & \
-                       self.metadata['malignant'].isin(malignant) & \
-                       self.metadata['disease'].isin(diseases))
-    selected_metadata = self.metadata.loc[indices]
-    # 4. create DDI_Dataset
-    self.dataset = DDI_Dataset(selected_metadata, img_dir, transform)
+        # parameters for DDI_DataSet
+        self.metadata = pd.read_csv(metadata_csv, index_col=0)  # './ddi_data/ddi_metadata.csv'
+        self.img_dir = img_dir  # './ddi_data/'
+        self.transform = transform
+
+        # Create dictionary mapping disease (str) to label (int)
+        self.disease2label = {disease: label for label, disease in enumerate(sorted(self.metadata['disease'].unique()))}
+        # Add 'disease_label' col to metadata with int label for each diagnosis
+        self.metadata['disease_label'] = self.metadata['disease'].map(self.disease2label)
+
+        # Create DDI_Dataset of specified skin tones, malignancy status, and diagnoses
+        # 1. parse inputs into lists for downstream processing
+        skin_tone = params_to_list(skin_tone, [12, 34, 56])
+        malignant = params_to_list(malignant, [True, False])
+        diseases = params_to_list(diseases, list(self.disease2label))
+        # 2. make sure specified inputs are valid
+        for s in skin_tone:
+            assert s in [12, 34, 56], f"{s} is not a valid skin tone"
+        for d in diseases:
+            assert d in self.disease2label, f"{d} is not a valid diagnosis"
+        # 3. select rows of metadata DataFrame that meet specifications
+        indices = np.where(self.metadata['skin_tone'].isin(skin_tone) &
+                           self.metadata['malignant'].isin(malignant) &
+                           self.metadata['disease'].isin(diseases))
+        selected_metadata = self.metadata.loc[indices]
+        # 4. create DDI_Dataset
+        self.dataset = DDI_Dataset(selected_metadata, img_dir, transform)
 
     def prepare_data(self):
-      pass
-    
-    def setup(self):
-      self.train_data, self.val_data, self.test_data = random_split(self.dataset, 
-                                                     [0.6, 0.2, 0.2], 
-                                                     generator=torch.Generator().manual_seed(self.seed))
-    
-    def train_dataloader(self):
-      return DataLoader(self.train_data, 
-                        batch_size=self.batch_size, 
-                        num_workers=self.num_workers, 
-                        pin_memory=True, 
-                        shuffle=True)
-    
-    def val_dataloader(self):
-      return DataLoader(self.val_data, 
-                        batch_size=self.batch_size, 
-                        num_workers=self.num_workers, 
-                        pin_memory=True, 
-                        shuffle=True)
-    
-    def test_dataloader(self):
-      return DataLoader(self.test_data, 
-                        batch_size=self.batch_size, 
-                        num_workers=self.num_workers, 
-                        pin_memory=True, 
-                        shuffle=True)
+        pass
 
+    def setup(self):
+        self.train_data, self.val_data, self.test_data = random_split(self.dataset,
+                                                                      [0.6, 0.2, 0.2],
+                                                                      generator=torch.Generator().manual_seed(
+                                                                          self.seed))
+
+    def train_dataloader(self):
+        return DataLoader(self.train_data,
+                          batch_size=self.batch_size,
+                          num_workers=self.num_workers,
+                          pin_memory=True,
+                          shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_data,
+                          batch_size=self.batch_size,
+                          num_workers=self.num_workers,
+                          pin_memory=True,
+                          shuffle=True)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_data,
+                          batch_size=self.batch_size,
+                          num_workers=self.num_workers,
+                          pin_memory=True,
+                          shuffle=True)
--- train.py
+++ train.py
@@ -1,71 +1,103 @@
 import argparse
+
+# pytorch
 import numpy as np
 import pandas as pd
-from torch.utils.data import DataLoader
-from ddi_dataset import DDI_DataModule, DDI_Dataset, params_to_list
-from ddi_models import DDI_DeepDerm, load_model
+import torch
+from torch.utils.data import random_split, DataLoader
+from datasets import DDI_Dataset, params_to_list
+from models import load_model
 
+# pytorchlightning
+import pytorch_lightning as pl
+from datasets import DDI_DataModule
+from models import DDI_DeepDerm
 
-def main(args):
-	# Parse CLI arguments
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--annotation_file', type=str, default='./ddi_data/ddi_metadata.csv')
-	parser.add_argument('--img_dir', type=str, default='./ddi_data/')
-	parser.add_argument('--batch_size', type=int, default=256)
-	parser.add_argument('--num_workers', type=int, default=4)
-	parser.add_argument('--random_seed', type=int, default=0)
-	parser.add_argument('--classify_malignant', type=bool, default=True)
-	args = parser.parse_args()
+def pytorch(args, config):
+    ### DATA PREPARATION ###
+    # Unpack config
+    transform, skin_tone, malignant, diseases = config
 
-	# Vanilla PyTorch
-	### DATA PREPARATION ###
+    # Read in metadata to DataFrame
+    annotations = pd.read_csv(args.annotation_file, index_col=0)
 
-	# Read in metadata to DataFrame
-	annotations = pd.read_csv(args.annotation_file, index_col=0)
+    # Map disease (string) to label (int)
+    disease2label = {disease: label for label, disease in enumerate(sorted(annotations['disease'].unique()))}
 
-	# Map disease (string) to label (int)
-	disease2label = {disease:label for label, disease in enumerate(sorted(annotations['disease'].unique()))}
-	
-	# Add 'disease_label' col to annotations with int label for each diagnosis
-	annotations['disease_label'] = annotations['disease'].map(disease2label)
-	
-	# Parse inputs into lists for downstream processing
-	skin_tone = params_to_list(skin_tone, [12, 34, 56])
-	malignant = params_to_list(malignant, [True, False])
-	diseases = params_to_list(diseases, list(disease2label))
-	
-	# Make sure specified inputs are valid
-	for s in skin_tone:
-		assert s in [12, 34, 56], f"{s} is not a valid skin tone"
-	for d in diseases:
-		assert d in disease2label, f"{d} is not a valid diagnosis"
-	
-	# Select rows of annotations DataFrame that meet specifications
-	indices = np.where(annotations['skin_tone'].isin(skin_tone) & \
-											annotations['malignant'].isin(malignant) & \
-											annotations['disease'].isin(diseases))
-	selected_data = annotations.loc[indices]
-	
-	# Create DDI_Dataset from selected data
-	full_dataset = DDI_Dataset(selected_data, args.img_dir, transform)
+    # Add 'disease_label' col to annotations with int label for each diagnosis
+    annotations['disease_label'] = annotations['disease'].map(disease2label)
 
-	# Split 60-20-20 (to match DDI) into train/val/test datasets based on random seed
-	train_data, val_data, test_data = random_split(self.dataset, 
-                                                     [0.6, 0.2, 0.2], 
-                                                     generator=torch.Generator().manual_seed(random_seed))
+    # Parse inputs into lists for downstream processing
+    skin_tone = params_to_list(skin_tone, [12, 34, 56])
+    malignant = params_to_list(malignant, [True, False])
+    diseases = params_to_list(diseases, list(disease2label))
 
-	# Initialize train/val/test dataloaders
-	train_loader = DataLoader(train_data, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True, shuffle=True)
-	val_loader = DataLoader(val_data, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True, shuffle=True)
-	test_loader = DataLoader(test_data, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True, shuffle=True)
+    # Make sure specified inputs are valid
+    for s in skin_tone:
+        assert s in [12, 34, 56], f"{s} is not a valid skin tone"
+    for d in diseases:
+        assert d in disease2label, f"{d} is not a valid diagnosis"
 
-	### MODEL SETUP ###
-	num_classes = 2 if classify_malignant else 78  # 78 diagnoses in DDI
-	model = load_model(model_name='DeepDerm', num_classes=num_classes)
+    # Select rows of annotations DataFrame that meet specifications
+    indices = np.where(annotations['skin_tone'].isin(skin_tone) &
+                       annotations['malignant'].isin(malignant) &
+                       annotations['disease'].isin(diseases))
+    selected_data = annotations.loc[indices]
 
-	# !!! TO DO !!!
-	### TRAINING LOOP ###
+    # Create DDI_Dataset from selected data
+    dataset = DDI_Dataset(selected_data, args.img_dir, transform)
 
+    # Split 60-20-20 (to match DDI) into train/val/test datasets based on random seed
+    train_data, val_data, test_data = random_split(dataset,
+                                                   [0.6, 0.2, 0.2],
+                                                   generator=torch.Generator().manual_seed(args.random_seed))
+
+    # Initialize train/val/test dataloaders
+    train_loader = DataLoader(train_data, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True, shuffle=True)
+
+    ### MODEL SETUP ###
+    num_classes = 2 if args.classify_malignant else 78  # 78 diagnoses in DDI
+    model = load_model(model_name='DeepDerm', num_classes=num_classes)
+
+    # !!! TO DO !!!
+    ### TRAINING LOOP ###
+
+def pytorchlightning(args, config):
+    # Unpack config
+    transform, skin_tone, malignant, diseases = config
+
+    data_module = DDI_DataModule(args.random_seed,
+                                 args.batch_size,
+                                 args.num_workers,
+                                 args.annotation_file,
+                                 args.img_dir,
+                                 transform,
+                                 skin_tone,
+                                 malignant,
+                                 diseases)
+    model = DDI_DeepDerm(classify_malignant=args.classify_malignant)
+    trainer = pl.Trainer(max_epochs=500)  # to match DDI experiments
+    trainer.fit(model, data_module)
 
 if __name__ == "__main__":
-	main()
+    # Parse CLI arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--annotation_file', type=str, default='./ddi_data/ddi_metadata.csv')
+    parser.add_argument('--img_dir', type=str, default='./ddi_data/')
+    parser.add_argument('--batch_size', type=int, default=256)
+    parser.add_argument('--num_workers', type=int, default=4)
+    parser.add_argument('--random_seed', type=int, default=0)
+    parser.add_argument('--classify_malignant', type=bool, default=True)
+    args = parser.parse_args()
+
+    # Config
+    transform = None  # TODO: match DDI experiments
+    skin_tone = None  # [12, 34, 56]
+    malignant = None  # [True, False]
+    diseases = None
+    config = (transform, skin_tone, malignant, diseases)
+
+    # pytorch(args, config)
+    # pytorchlightning(args, config)
